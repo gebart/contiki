f8e41c08cd97137877fcb02c9791cd220d752235
Henrik Mäkitaavola <henrik.makitaavola@gmail.com>
2014-02-28 10:29:19 +0100

    Fixed installation scripts and removed contiki setup script because code now lies in repo.

--- platform/mulle/tools/setup_contiki.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget http://downloads.sourceforge.net/project/contiki/Contiki/Contiki%202.7/contiki-2.7.zip
-unzip contiki-2.7.zip
-rm contiki-2.7.zip
-mv contiki-2.7 contiki
-
-echo "Create link for Mulle platform into Contiki platforms"
-ln -s `pwd`/platform contiki/platform/mulle
