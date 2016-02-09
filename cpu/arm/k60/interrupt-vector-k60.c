809b9e7cfa3554f93fbe3d610a521b20a1361564
Joakim Gebart <joakim.gebart@eistec.se>
2016-02-09 10:36:45 +0100

    cpu/arm/k60: Rename ISRs to match naming in RIOT

--- cpu/arm/k60/interrupt-vector-k60.c
+++ cpu/arm/k60/interrupt-vector-k60.c
@@ -12,11 +12,11 @@
 #define SECTION(x) __attribute__ ((section(#x)))
 #define ISR_VECTOR_SECTION SECTION(.vector_table)
 void reset_handler(void) __attribute__((naked));
-void _isr_nmi(void) __attribute__((interrupt));
-void _isr_hardfault(void) __attribute__((interrupt));
-void _isr_memmanage(void) __attribute__((interrupt));
-void _isr_busfault(void) __attribute__((interrupt));
-void _isr_usagefault(void) __attribute__((interrupt));
+void isr_nmi(void) __attribute__((interrupt));
+void isr_hardfault(void) __attribute__((interrupt));
+void isr_memmanage(void) __attribute__((interrupt));
+void isr_busfault(void) __attribute__((interrupt));
+void isr_usagefault(void) __attribute__((interrupt));
 
 /* Default handler for interrupts, infinite loop */
 static void unhandled_interrupt(void) __attribute__((interrupt, unused));
@@ -34,117 +34,117 @@ static void dBusFault_handler(void) __attribute__((unused));
 
 /* ARM Cortex defined interrupt vectors */
 void reset_handler(void) __attribute__((naked));
-void _isr_nmi(void) __attribute__((interrupt));
-void _isr_hardfault(void) __attribute__((weak, alias("dHardFault_handler")));
-void _isr_memmanage(void) __attribute__((weak, alias("dMemManage_handler")));
-void _isr_busfault(void) __attribute__((weak, alias("dBusFault_handler")));
-void _isr_usagefault(void) __attribute__((weak, alias("dUsageFault_handler")));
-void _isr_reserved(void) UNHANDLED_ALIAS;
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-void _isr_svcall(void) UNHANDLED_ALIAS;
-void _isr_debugmonitor(void) UNHANDLED_ALIAS;
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-void _isr_pendsv(void) UNHANDLED_ALIAS;
-void _isr_systick(void) UNHANDLED_ALIAS;
+void isr_nmi(void) __attribute__((interrupt));
+void isr_hardfault(void) __attribute__((weak, alias("dHardFault_handler")));
+void isr_memmanage(void) __attribute__((weak, alias("dMemManage_handler")));
+void isr_busfault(void) __attribute__((weak, alias("dBusFault_handler")));
+void isr_usagefault(void) __attribute__((weak, alias("dUsageFault_handler")));
+void isr_reserved(void) UNHANDLED_ALIAS;
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+void isr_svcall(void) UNHANDLED_ALIAS;
+void isr_debugmonitor(void) UNHANDLED_ALIAS;
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+void isr_pendsv(void) UNHANDLED_ALIAS;
+void isr_systick(void) UNHANDLED_ALIAS;
 
 /* device-specific (freescale) defined interrupt vectors */
-void _isr_dma0_complete(void) UNHANDLED_ALIAS;
-void _isr_dma1_complete(void) UNHANDLED_ALIAS;
-void _isr_dma2_complete(void) UNHANDLED_ALIAS;
-void _isr_dma3_complete(void) UNHANDLED_ALIAS;
-void _isr_dma4_complete(void) UNHANDLED_ALIAS;
-void _isr_dma5_complete(void) UNHANDLED_ALIAS;
-void _isr_dma6_complete(void) UNHANDLED_ALIAS;
-void _isr_dma7_complete(void) UNHANDLED_ALIAS;
-void _isr_dma8_complete(void) UNHANDLED_ALIAS;
-void _isr_dma9_complete(void) UNHANDLED_ALIAS;
-void _isr_dma10_complete(void) UNHANDLED_ALIAS;
-void _isr_dma11_complete(void) UNHANDLED_ALIAS;
-void _isr_dma12_complete(void) UNHANDLED_ALIAS;
-void _isr_dma13_complete(void) UNHANDLED_ALIAS;
-void _isr_dma14_complete(void) UNHANDLED_ALIAS;
-void _isr_dma15_complete(void) UNHANDLED_ALIAS;
-void _isr_dma_error(void) UNHANDLED_ALIAS;
-void _isr_mcm(void) UNHANDLED_ALIAS;
-void _isr_flash_command_complete(void) UNHANDLED_ALIAS;
-void _isr_flash_read_collision(void) UNHANDLED_ALIAS;
-void _isr_low_voltage(void) UNHANDLED_ALIAS;
-void _isr_llwu(void) UNHANDLED_ALIAS;
-void _isr_watchdog(void) UNHANDLED_ALIAS;
-void _isr_random_number_generator(void) UNHANDLED_ALIAS;
-void _isr_i2c0(void) UNHANDLED_ALIAS;
-void _isr_i2c1(void) UNHANDLED_ALIAS;
-void _isr_spi0(void) UNHANDLED_ALIAS;
-void _isr_spi1(void) UNHANDLED_ALIAS;
-void _isr_spi2(void) UNHANDLED_ALIAS;
-void _isr_can0_ored_msg_buffer(void) UNHANDLED_ALIAS;
-void _isr_can0_bus_off(void) UNHANDLED_ALIAS;
-void _isr_can0_error(void) UNHANDLED_ALIAS;
-void _isr_can0_tx_warn(void) UNHANDLED_ALIAS;
-void _isr_can0_rx_warn(void) UNHANDLED_ALIAS;
-void _isr_can0_wake_up(void) UNHANDLED_ALIAS;
-void _isr_i2s0_tx(void) UNHANDLED_ALIAS;
-void _isr_i2s0_rx(void) UNHANDLED_ALIAS;
-void _isr_can1_ored_msg_buffer(void) UNHANDLED_ALIAS;
-void _isr_can1_bus_off(void) UNHANDLED_ALIAS;
-void _isr_can1_error(void) UNHANDLED_ALIAS;
-void _isr_can1_tx_warn(void) UNHANDLED_ALIAS;
-void _isr_can1_rx_warn(void) UNHANDLED_ALIAS;
-void _isr_can1_wake_up(void) UNHANDLED_ALIAS;
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-void _isr_uart0_lon(void) UNHANDLED_ALIAS;
-void _isr_uart0_status(void) UNHANDLED_ALIAS;
-void _isr_uart0_error(void) UNHANDLED_ALIAS;
-void _isr_uart1_status(void) UNHANDLED_ALIAS;
-void _isr_uart1_error(void) UNHANDLED_ALIAS;
-void _isr_uart2_status(void) UNHANDLED_ALIAS;
-void _isr_uart2_error(void) UNHANDLED_ALIAS;
-void _isr_uart3_status(void) UNHANDLED_ALIAS;
-void _isr_uart3_error(void) UNHANDLED_ALIAS;
-void _isr_uart4_status(void) UNHANDLED_ALIAS;
-void _isr_uart4_error(void) UNHANDLED_ALIAS;
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-void _isr_adc0(void) UNHANDLED_ALIAS;
-void _isr_adc1(void) UNHANDLED_ALIAS;
-void _isr_cmp0(void) UNHANDLED_ALIAS;
-void _isr_cmp1(void) UNHANDLED_ALIAS;
-void _isr_cmp2(void) UNHANDLED_ALIAS;
-void _isr_ftm0(void) UNHANDLED_ALIAS;
-void _isr_ftm1(void) UNHANDLED_ALIAS;
-void _isr_ftm2(void) UNHANDLED_ALIAS;
-void _isr_cmt(void) UNHANDLED_ALIAS;
-void _isr_rtc_alarm(void) UNHANDLED_ALIAS;
-void _isr_rtc_seconds(void) UNHANDLED_ALIAS;
-void _isr_pit0(void) UNHANDLED_ALIAS;
-void _isr_pit1(void) UNHANDLED_ALIAS;
-void _isr_pit2(void) UNHANDLED_ALIAS;
-void _isr_pit3(void) UNHANDLED_ALIAS;
-void _isr_pdb(void) UNHANDLED_ALIAS;
-void _isr_usb_otg(void) UNHANDLED_ALIAS;
-void _isr_usb_charger_detect(void) UNHANDLED_ALIAS;
-void _isr_enet_1588_timer(void) UNHANDLED_ALIAS;
-void _isr_enet_tx(void) UNHANDLED_ALIAS;
-void _isr_enet_rx(void) UNHANDLED_ALIAS;
-void _isr_enet_error_misc(void) UNHANDLED_ALIAS;
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-void _isr_sdhc(void) UNHANDLED_ALIAS;
-void _isr_dac0(void) UNHANDLED_ALIAS;
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-void _isr_tsi(void) UNHANDLED_ALIAS;
-void _isr_mcg(void) UNHANDLED_ALIAS;
-void _isr_lpt(void) UNHANDLED_ALIAS;
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-void _isr_porta_pin_detect(void) UNHANDLED_ALIAS;
-void _isr_portb_pin_detect(void) UNHANDLED_ALIAS;
-void _isr_portc_pin_detect(void) UNHANDLED_ALIAS;
-void _isr_portd_pin_detect(void) UNHANDLED_ALIAS;
-void _isr_porte_pin_detect(void) UNHANDLED_ALIAS;
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-/* void _isr_reserved(void) UNHANDLED_ALIAS; */
-void _isr_software(void) UNHANDLED_ALIAS;
+void isr_dma0_complete(void) UNHANDLED_ALIAS;
+void isr_dma1_complete(void) UNHANDLED_ALIAS;
+void isr_dma2_complete(void) UNHANDLED_ALIAS;
+void isr_dma3_complete(void) UNHANDLED_ALIAS;
+void isr_dma4_complete(void) UNHANDLED_ALIAS;
+void isr_dma5_complete(void) UNHANDLED_ALIAS;
+void isr_dma6_complete(void) UNHANDLED_ALIAS;
+void isr_dma7_complete(void) UNHANDLED_ALIAS;
+void isr_dma8_complete(void) UNHANDLED_ALIAS;
+void isr_dma9_complete(void) UNHANDLED_ALIAS;
+void isr_dma10_complete(void) UNHANDLED_ALIAS;
+void isr_dma11_complete(void) UNHANDLED_ALIAS;
+void isr_dma12_complete(void) UNHANDLED_ALIAS;
+void isr_dma13_complete(void) UNHANDLED_ALIAS;
+void isr_dma14_complete(void) UNHANDLED_ALIAS;
+void isr_dma15_complete(void) UNHANDLED_ALIAS;
+void isr_dma_error(void) UNHANDLED_ALIAS;
+void isr_mcm(void) UNHANDLED_ALIAS;
+void isr_flash_command_complete(void) UNHANDLED_ALIAS;
+void isr_flash_read_collision(void) UNHANDLED_ALIAS;
+void isr_low_voltage(void) UNHANDLED_ALIAS;
+void isr_llwu(void) UNHANDLED_ALIAS;
+void isr_watchdog(void) UNHANDLED_ALIAS;
+void isr_random_number_generator(void) UNHANDLED_ALIAS;
+void isr_i2c0(void) UNHANDLED_ALIAS;
+void isr_i2c1(void) UNHANDLED_ALIAS;
+void isr_spi0(void) UNHANDLED_ALIAS;
+void isr_spi1(void) UNHANDLED_ALIAS;
+void isr_spi2(void) UNHANDLED_ALIAS;
+void isr_can0_ored_msg_buffer(void) UNHANDLED_ALIAS;
+void isr_can0_bus_off(void) UNHANDLED_ALIAS;
+void isr_can0_error(void) UNHANDLED_ALIAS;
+void isr_can0_tx_warn(void) UNHANDLED_ALIAS;
+void isr_can0_rx_warn(void) UNHANDLED_ALIAS;
+void isr_can0_wake_up(void) UNHANDLED_ALIAS;
+void isr_i2s0_tx(void) UNHANDLED_ALIAS;
+void isr_i2s0_rx(void) UNHANDLED_ALIAS;
+void isr_can1_ored_msg_buffer(void) UNHANDLED_ALIAS;
+void isr_can1_bus_off(void) UNHANDLED_ALIAS;
+void isr_can1_error(void) UNHANDLED_ALIAS;
+void isr_can1_tx_warn(void) UNHANDLED_ALIAS;
+void isr_can1_rx_warn(void) UNHANDLED_ALIAS;
+void isr_can1_wake_up(void) UNHANDLED_ALIAS;
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+void isr_uart0_lon(void) UNHANDLED_ALIAS;
+void isr_uart0_status(void) UNHANDLED_ALIAS;
+void isr_uart0_error(void) UNHANDLED_ALIAS;
+void isr_uart1_status(void) UNHANDLED_ALIAS;
+void isr_uart1_error(void) UNHANDLED_ALIAS;
+void isr_uart2_status(void) UNHANDLED_ALIAS;
+void isr_uart2_error(void) UNHANDLED_ALIAS;
+void isr_uart3_status(void) UNHANDLED_ALIAS;
+void isr_uart3_error(void) UNHANDLED_ALIAS;
+void isr_uart4_status(void) UNHANDLED_ALIAS;
+void isr_uart4_error(void) UNHANDLED_ALIAS;
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+void isr_adc0(void) UNHANDLED_ALIAS;
+void isr_adc1(void) UNHANDLED_ALIAS;
+void isr_cmp0(void) UNHANDLED_ALIAS;
+void isr_cmp1(void) UNHANDLED_ALIAS;
+void isr_cmp2(void) UNHANDLED_ALIAS;
+void isr_ftm0(void) UNHANDLED_ALIAS;
+void isr_ftm1(void) UNHANDLED_ALIAS;
+void isr_ftm2(void) UNHANDLED_ALIAS;
+void isr_cmt(void) UNHANDLED_ALIAS;
+void isr_rtc_alarm(void) UNHANDLED_ALIAS;
+void isr_rtc_seconds(void) UNHANDLED_ALIAS;
+void isr_pit0(void) UNHANDLED_ALIAS;
+void isr_pit1(void) UNHANDLED_ALIAS;
+void isr_pit2(void) UNHANDLED_ALIAS;
+void isr_pit3(void) UNHANDLED_ALIAS;
+void isr_pdb(void) UNHANDLED_ALIAS;
+void isr_usb_otg(void) UNHANDLED_ALIAS;
+void isr_usb_charger_detect(void) UNHANDLED_ALIAS;
+void isr_enet_1588_timer(void) UNHANDLED_ALIAS;
+void isr_enet_tx(void) UNHANDLED_ALIAS;
+void isr_enet_rx(void) UNHANDLED_ALIAS;
+void isr_enet_error_misc(void) UNHANDLED_ALIAS;
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+void isr_sdhc(void) UNHANDLED_ALIAS;
+void isr_dac0(void) UNHANDLED_ALIAS;
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+void isr_tsi(void) UNHANDLED_ALIAS;
+void isr_mcg(void) UNHANDLED_ALIAS;
+void isr_lpt(void) UNHANDLED_ALIAS;
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+void isr_porta_pin_detect(void) UNHANDLED_ALIAS;
+void isr_portb_pin_detect(void) UNHANDLED_ALIAS;
+void isr_portc_pin_detect(void) UNHANDLED_ALIAS;
+void isr_portd_pin_detect(void) UNHANDLED_ALIAS;
+void isr_porte_pin_detect(void) UNHANDLED_ALIAS;
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+/* void isr_reserved(void) UNHANDLED_ALIAS; */
+void isr_software(void) UNHANDLED_ALIAS;
 
 /* This is the value of the stack pointer at power on. This is not a real function. */
 void __stack_top(void);
@@ -156,121 +156,266 @@ const ISR_func isr_vector[111] __attribute__((used)) ISR_VECTOR_SECTION =
   /* ARM Cortex defined interrupt vectors */
   __stack_top,
   reset_handler,
-  _isr_nmi,
-  _isr_hardfault,
-  _isr_memmanage,
-  _isr_busfault,
-  _isr_usagefault,
-  _isr_reserved,
-  _isr_reserved,
-  _isr_reserved,
-  _isr_reserved,
-  _isr_svcall,
-  _isr_debugmonitor,
-  _isr_reserved,
-  _isr_pendsv,
-  _isr_systick,
+  isr_nmi,
+  isr_hardfault,
+  isr_memmanage,
+  isr_busfault,
+  isr_usagefault,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_svc,
+  isr_debugmonitor,
+  isr_reserved,
+  isr_pendsv,
+  isr_systick,
 
   /* Device-specific (Freescale defined) interrupt vectors */
-  _isr_dma0_complete,
-  _isr_dma1_complete,
-  _isr_dma2_complete,
-  _isr_dma3_complete,
-  _isr_dma4_complete,
-  _isr_dma5_complete,
-  _isr_dma6_complete,
-  _isr_dma7_complete,
-  _isr_dma8_complete,
-  _isr_dma9_complete,
-  _isr_dma10_complete,
-  _isr_dma11_complete,
-  _isr_dma12_complete,
-  _isr_dma13_complete,
-  _isr_dma14_complete,
-  _isr_dma15_complete,
-  _isr_dma_error,
-  _isr_mcm,
-  _isr_flash_command_complete,
-  _isr_flash_read_collision,
-  _isr_low_voltage,
-  _isr_llwu,
-  _isr_watchdog,
-  _isr_random_number_generator,
-  _isr_i2c0,
-  _isr_i2c1,
-  _isr_spi0,
-  _isr_spi1,
-  _isr_spi2,
-  _isr_can0_ored_msg_buffer,
-  _isr_can0_bus_off,
-  _isr_can0_error,
-  _isr_can0_tx_warn,
-  _isr_can0_rx_warn,
-  _isr_can0_wake_up,
-  _isr_i2s0_tx,
-  _isr_i2s0_rx,
-  _isr_can1_ored_msg_buffer,
-  _isr_can1_bus_off,
-  _isr_can1_error,
-  _isr_can1_tx_warn,
-  _isr_can1_rx_warn,
-  _isr_can1_wake_up,
-  _isr_reserved,
-  _isr_uart0_lon,
-  _isr_uart0_status,
-  _isr_uart0_error,
-  _isr_uart1_status,
-  _isr_uart1_error,
-  _isr_uart2_status,
-  _isr_uart2_error,
-  _isr_uart3_status,
-  _isr_uart3_error,
-  _isr_uart4_status,
-  _isr_uart4_error,
-  _isr_reserved,
-  _isr_reserved,
-  _isr_adc0,
-  _isr_adc1,
-  _isr_cmp0,
-  _isr_cmp1,
-  _isr_cmp2,
-  _isr_ftm0,
-  _isr_ftm1,
-  _isr_ftm2,
-  _isr_cmt,
-  _isr_rtc_alarm,
-  _isr_rtc_seconds,
-  _isr_pit0,
-  _isr_pit1,
-  _isr_pit2,
-  _isr_pit3,
-  _isr_pdb,
-  _isr_usb_otg,
-  _isr_usb_charger_detect,
-  _isr_enet_1588_timer,
-  _isr_enet_tx,
-  _isr_enet_rx,
-  _isr_enet_error_misc,
-  _isr_reserved,
-  _isr_sdhc,
-  _isr_dac0,
-  _isr_reserved,
-  _isr_tsi,
-  _isr_mcg,
-  _isr_lpt,
-  _isr_reserved,
-  _isr_porta_pin_detect,
-  _isr_portb_pin_detect,
-  _isr_portc_pin_detect,
-  _isr_portd_pin_detect,
-  _isr_porte_pin_detect,
-  _isr_reserved,
-  _isr_reserved,
-  _isr_software
+  isr_dma0_complete,
+  isr_dma1_complete,
+  isr_dma2_complete,
+  isr_dma3_complete,
+  isr_dma4_complete,
+  isr_dma5_complete,
+  isr_dma6_complete,
+  isr_dma7_complete,
+  isr_dma8_complete,
+  isr_dma9_complete,
+  isr_dma10_complete,
+  isr_dma11_complete,
+  isr_dma12_complete,
+  isr_dma13_complete,
+  isr_dma14_complete,
+  isr_dma15_complete,
+  isr_dma_error,
+  isr_mcm,
+  isr_flash_command_complete,
+  isr_flash_read_collision,
+  isr_low_voltage,
+  isr_llwu,
+  isr_watchdog,
+  isr_random_number_generator,
+  isr_i2c0,
+  isr_i2c1,
+  isr_spi0,
+  isr_spi1,
+  isr_spi2,
+  isr_can0_ored_msg_buffer,
+  isr_can0_bus_off,
+  isr_can0_error,
+  isr_can0_tx_warn,
+  isr_can0_rx_warn,
+  isr_can0_wake_up,
+  isr_i2s0_tx,
+  isr_i2s0_rx,
+  isr_can1_ored_msg_buffer,
+  isr_can1_bus_off,
+  isr_can1_error,
+  isr_can1_tx_warn,
+  isr_can1_rx_warn,
+  isr_can1_wake_up,
+  isr_reserved,
+  isr_uart0_lon,
+  isr_uart0_status,
+  isr_uart0_error,
+  isr_uart1_status,
+  isr_uart1_error,
+  isr_uart2_status,
+  isr_uart2_error,
+  isr_uart3_status,
+  isr_uart3_error,
+  isr_uart4_status,
+  isr_uart4_error,
+  isr_reserved,
+  isr_reserved,
+  isr_adc0,
+  isr_adc1,
+  isr_cmp0,
+  isr_cmp1,
+  isr_cmp2,
+  isr_ftm0,
+  isr_ftm1,
+  isr_ftm2,
+  isr_cmt,
+  isr_rtc_alarm,
+  isr_rtc_seconds,
+  isr_pit0,
+  isr_pit1,
+  isr_pit2,
+  isr_pit3,
+  isr_pdb,
+  isr_usb_otg,
+  isr_usb_charger_detect,
+  isr_enet_1588_timer,
+  isr_enet_tx,
+  isr_enet_rx,
+  isr_enet_error_misc,
+  isr_reserved,
+  isr_sdhc,
+  isr_dac0,
+  isr_reserved,
+  isr_tsi,
+  isr_mcg,
+  isr_lptmr0,
+  isr_reserved,
+  isr_porta,
+  isr_portb,
+  isr_portc,
+  isr_portd,
+  isr_porte,
+  isr_reserved,
+  isr_reserved,
+  isr_software, /* vector 110 */
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved,
+  isr_reserved /* vector 255 */
 };
 
 void
-_isr_nmi(void)
+isr_nmi(void)
 {
   while(1);
 }
