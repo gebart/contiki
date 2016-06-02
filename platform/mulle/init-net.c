a0681ee3aa1aefec4940afa748ed231b645f7e06
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-06-02 11:27:58 +0200

    mulle/init-net: add #if WITH_IPSO around lwm2m includes

--- platform/mulle/init-net.c
+++ platform/mulle/init-net.c
@@ -47,8 +47,10 @@
 #include "at86rf212.h"
 #include "net/netstack.h"
 #include "net/mac/frame802154.h"
+#if WITH_IPSO
 #include "lwm2m-engine.h"
 #include "ipso-objects.h"
+#endif
 
 #define DEBUG 1
 #include "net/ip/uip-debug.h"
