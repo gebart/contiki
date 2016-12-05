2b58efa2aecf63ed40cfec65c82d8b1cb5e4099a
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-12-05 21:28:55 +0100

    k60/adc: Fix wrong prescaler configuration

--- cpu/arm/k60/drivers/RIOT/adc.c
+++ cpu/arm/k60/drivers/RIOT/adc.c
@@ -170,16 +170,16 @@ int adc_init(adc_t line)
      * than 12 MHz */
     /* For the calibration it is important that the ADC clock is <= 4 MHz */
     uint32_t adiv;
-    int i = 4;
     if (CLOCK_BUSCLOCK > (ADC_MAX_CLK * 8)) {
         /* Maximum clock division */
         adiv = ADC_CFG1_ADIV(3) | ADC_CFG1_ADICLK(1);
     }
     else {
-        while ((i > 0) && (CLOCK_BUSCLOCK < (ADC_MAX_CLK << i))) {
-            i--;
+        unsigned int i = 0;
+        while ((i < 3) && (CLOCK_BUSCLOCK > (ADC_MAX_CLK << i))) {
+            ++i;
         }
-        adiv = ADC_CFG1_ADIV(i - 1);
+        adiv = ADC_CFG1_ADIV(i);
     }
 
     /* set configuration register 1: clocking and precision */
